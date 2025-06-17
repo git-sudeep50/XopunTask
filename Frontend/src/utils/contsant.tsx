95703b4704d334fd76d7239e85cc18e526985013
Sakil1246 <sakilahmed345677@gmail.com>
2025-06-18 03:15:28 +0530

    added signUp

--- Frontend/src/utils/contsant.tsx
+++ Frontend/src/utils/contsant.tsx
@@ -1,2 +1,4 @@
-export const SignUp_IMG='https://media.istockphoto.com/id/1279502184/photo/project-management-concept-with-gantt-chart.jpg?s=612x612&w=0&k=20&c=cyH6eJgMjZPuhYwCp1mc334Y3EOngkPAibyBBokJ7To='
-export const BG_IMG='https://t3.ftcdn.net/jpg/03/09/78/04/360_F_309780437_bztYRNRTMDZYw3tYMSvxY3taQcE7mJqE.jpg'
+export const SignUp_IMG1='https://media.istockphoto.com/id/1279502184/photo/project-management-concept-with-gantt-chart.jpg?s=612x612&w=0&k=20&c=cyH6eJgMjZPuhYwCp1mc334Y3EOngkPAibyBBokJ7To='
+export const BG_IMG='https://img.haikudeck.com/mi/8FAF9009-A9E3-4ED7-AE01-6F283BAFB63D.jpg'
+export const SignUp_IMG="https://t4.ftcdn.net/jpg/10/84/08/15/360_F_1084081551_lDWEhMTo9PLIdxGTacdd0IMUzmROxW42.jpg"
+export const BASE_URL="http://localhost:7000/"
