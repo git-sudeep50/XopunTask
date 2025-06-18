1ece832708dacd6973a6a105398ddb78118e5483
Sakil1246 <sakilahmed345677@gmail.com>
2025-06-18 17:05:15 +0530

    sidebar added

---
+++ Frontend/src/pages/Home.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideBar from '../Components/SideBar'
+
+const Homepage = () => {
+  return (
+    <div className='flex flex-row h-screen w-screen bg-gray-100'>
+        <div className=' w-full h-full'>
+        <SideBar/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Homepage
